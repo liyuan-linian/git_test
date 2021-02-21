a8805b2c17d49fa90c748604ffdb4a34a75cc8ef
gyf <2892068996@qq.com>
2021-02-21 19:14:33 +0800

    Commit Message

---
+++ cmake-build-debug/CMakeFiles/git_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_test.dir/main.c.obj"
+  "git_test.exe"
+  "git_test.exe.manifest"
+  "git_test.pdb"
+  "libgit_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/git_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
